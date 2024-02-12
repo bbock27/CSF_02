2bfe3a1722bca9aea7d8101ac9c729af2aab1dc9
bbock27 <bradybock11@icloud.com>
2024-02-12 16:44:09 -0500

    fixed small bug with test case

--- c_drawing_funcs.c
+++ c_drawing_funcs.c
@@ -16,11 +16,13 @@
 // TODO: implement helper functions
 
 int32_t in_bounds(struct Image *img, int32_t x, int32_t y) {
+	
   return ((x >= 0 && x < img->width) && (y >= 0 && y < img->height)); // do they want a boolean or int32_t...?
 }
 
 uint32_t compute_index(struct Image *img, int32_t x, int32_t y) {
-  return ((img->height - y - 1) * img->width) + x; // double cbheck
+	return img->width*y + x;
+  // return ((img->height - y - 1) * img->width) + x; // double cbheck
 }
 
 int32_t clamp(int32_t val, int32_t min, int32_t max) {
